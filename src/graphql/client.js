e5715653ed410920bbbb200d41a1b94c1d90147c
SecondThrow <malexan0@gmail.com>
2019-12-13 01:21:56 -0500

    Remodeled Okta authentication for backend: https://developer.okta.com/docs/guides/protect-your-api/nodeexpress/before-you-begin/ Backend should serve dummy user if not logged in, user data if logged in
    Started frontend login code to get and store tokens: https://developer.okta.com/docs/guides/sign-into-spa/react/before-you-begin/

--- src/graphql/client.js
+++ src/graphql/client.js
@@ -2,5 +2,18 @@ import ApolloClient, { InMemoryCache } from 'apollo-boost';
 
 const BASE_URL = 'http://localhost:4000/';
 
-const client = new ApolloClient({ uri: BASE_URL, cache: new InMemoryCache(), connectToDevTools: true });
+const client = new ApolloClient({
+    uri: BASE_URL,
+    cache: new InMemoryCache(),
+    connectToDevTools: true,
+    // TODO: Work with Mijeong on the client-side login/logout modals and test (should be done at that point)
+    // request: operation => {
+    //     const token = localStorage.getItem('token');
+    //     operation.setContext({
+    //         headers: {
+    //             authorization: token ? `Bearer ${token}` : undefined
+    //         },
+    //     });
+    // },
+});
 export default client;
