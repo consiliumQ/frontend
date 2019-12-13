06153f1f37ecc2879b91e46d69e24588d0e85f77
Mijeong Ban <mban1@stevens.edu>
2019-12-13 12:23:20 -0500

    update one column is working

--- src/components/DeleteColumnAlertDialog.js
+++ src/components/DeleteColumnAlertDialog.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Dialog, DialogActions, DialogTitle, DialogContent, DialogContentText, Button } from '@material-ui/core';
-import { useMutation } from '@apollo/react-hooks';
-import { queries } from '../graphql';
+// import { useMutation } from '@apollo/react-hooks';
+// import { queries } from '../graphql';
 import Transition from './common/TransitionEffect';
 
 export default function DeleteColumnAlertDialog({ columnId, shouldDeleteColumnDialogOpen, handleDeleteColumnDialogToggle }) {
-    const [deleteColumnMutation] = useMutation(queries.MUTATE_DELETE_COLUMN);
+    // const [deleteColumnMutation] = useMutation(queries.MUTATE_DELETE_COLUMN);
 
-    const onConfirmButtonClick = () => {
-        deleteColumnMutation({ variables: { columnId } });
-        handleDeleteColumnDialogToggle();
-    };
+    // const onConfirmButtonClick = () => {
+    //     deleteColumnMutation({ variables: { columnId } });
+    //     handleDeleteColumnDialogToggle();
+    // };
 
     return (
         <Dialog open={shouldDeleteColumnDialogOpen} onClose={handleDeleteColumnDialogToggle} TransitionComponent={Transition}>
@@ -19,9 +19,7 @@ export default function DeleteColumnAlertDialog({ columnId, shouldDeleteColumnDi
                 <DialogContentText>Are you sure you want to delete this column?</DialogContentText>
             </DialogContent>
             <DialogActions>
-                <Button onClick={() => onConfirmButtonClick()} color="primary">
-                    Yes, I am sure
-                </Button>
+                <Button color="primary">Yes, I am sure</Button>
                 <Button onClick={handleDeleteColumnDialogToggle} color="primary">
                     Cancel
                 </Button>
